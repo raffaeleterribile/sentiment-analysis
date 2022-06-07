bb39d241e9a41fb842be1904249e6d908f3b2d3f
Raffaele Terribile <raff-github@usa.net>
2022-06-07 19:52:40 +0200

    feature: Complete exercise

--- serve/predict.py
+++ serve/predict.py
@@ -70,8 +70,7 @@ def predict_fn(input_data, model):
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
-    data_X = convert_and_pad(review_to_words(input_data))
-    data_len = len(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -87,6 +86,6 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = round(model(input_data))
+    result = model(data).round().detach().numpy()
 
     return result
